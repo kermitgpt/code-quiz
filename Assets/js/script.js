1342829ecf0fb1c261777928007aa0c24a572f77
Matt Grogan <mattgrogan.dev@gmail.com>
2021-11-11 19:31:57 -0700

    added area for quiz questions, start, and end sections for quiz

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,12 @@
 //VARIABLES
+var startQuiz = $("#startQuizBtn");
+var score = 0;
+var questionPos = 0;
 
 var userName = $("#userName");
 var userScore = $("#userScore");
 var playAgain = $("#playAgain");
+var countdown = $("#countdownClock");
 
 //FUNCTIONS
 
